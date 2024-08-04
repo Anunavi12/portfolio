2514d6b30edc65ebad3eb09e46dd8cbf7b54ab91
Anunavi12 <143801243+Anunavi12@users.noreply.github.com>
2024-08-04 18:44:48 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,47 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Nithin';
+
+export const experiences = [
+    {
+        'company': 'Inncircles Technologies',
+        'role': 'Product Developer',
+        'duration': 'April 2023 - Present',
+        'logo': 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
+        'points': [
+            'Involved in dynamic contributions to web and mobile📱 development projects, actively influencing and improving project outcomes.',
+            'Developed multiple shared components to simplify tasks for fellow developers, boosting efficiency and teamwork.',
+            'Enhanced product performance through significant contributions to backend development, optimizing essential components for improved efficiency 📈.',
+            'Implemented data security for the company’s product to provide role-based access control ⚙️.'
+        ],
+        'url': 'https://inncircles.com/#home',
+    },
+    {
+        'company': 'KPIT',
+        'role': 'Technical Intern',
+        'duration': 'May 2024 - July 2024',
+        'logo': 'https://yt3.ggpht.com/a-/AN66SAzhheBneLCPLpwxcG3QP6v8y7AJF_LWI_HoyQ=s900-mo-c-c0xffffffff-rj-k-no',
+        'points': [
+            'During my internship, I developed an intelligent seat belt system for vehicles, integrating sensors for occupancy detection, seat belt status, and posture monitoring.',
+            'I gained hands-on experience with Raspberry Pi Pico, MicroPython, and real-time alert systems.',
+            'This project enhanced my skills in embedded systems, sensor integration, and safety-critical applications📱.'
+        ],
+        'url': 'https://www.kpit.com/',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
